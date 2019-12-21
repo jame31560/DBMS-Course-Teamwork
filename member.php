04dda8f752871674d3778c267dbe2a62b9c16ee2
NiKaHou <willi927927@gmail.com>
2019-12-21 21:02:57 +0800

    張峻瑋
    取得到值但是用不到下拉式選單

--- member.php
+++ member.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title> YuntechEat </title>
     <?php
-    $conn = new mysqli("localhost", "root", "123", "deliverysystem") or die("連接失敗");
+    $conn = new mysqli("localhost", "root", "", "deliverysystem") or die("連接失敗");
 
     $dbname = "deliverysystem";
     $conn->query("SET NAMES utf8");
@@ -15,20 +15,36 @@
 </head>
 
 <body>
-
-    <h1>YuntechEat 會員管理頁面</h1>
-    <form id="form1" name="form1" method="post" action="">
-        <label>請選擇會員名稱
-            <select name="select">
-                <?php
-    $str="SELECT name FROM member ";
+    <?php
     $result = $conn->query($sql);
-    while(list($name) = mysql_fetch_row($result))
-    {
-    echo "<option value=".$name."">"</option>\n";
+
+    if ($conn->errno != 0){ // 查詢失敗
+    echo "錯誤代碼: ".odbc_error($link)."<br/>";
+    echo "錯誤訊息: ".odbc_errormsg($link)."<br/>"; 
     }
+    else{
+    $col_2 = [];
+    while ($row = $result->fetch_row()){
+        print_r($row);
+        array_push($col_2, $row);
+    }
+    echo (count($col_2)) ;
+    echo $col_2[1][0];    
+}
     ?>
-            </select>
+    <h1>YuntechEat 會員管理頁面</h1>
+    <label>請選擇會員名稱
+        <select name="month">
+            <?php
+		$month_select = array('', '1', '2', '3', '4', '5', 
+					'6', '7', '8', '9', '10','11', '12');
+        for($i = 0 ; $i < count($col_2);$i++){
+            $name = $col_2[i][0];
+            echo ("<option value=\"$name\">$name</option>");
+        }	
+		
+	?>
+        </select>
 
 </body>
 
