1d3b03a68518640f37286a3a4c9f4ed8ce5ddada
JaoJao <jame31560@gmail.com>
2019-12-27 16:20:26 +0800

    change frontend

--- member.php
+++ member.php
@@ -52,6 +52,7 @@
     }
   ?>
     <h1 class="text-center mt-2">YuntechEat 會員管理頁面</h1>
+    <hr>
     <?php
       if ($hasMsg) {
         $msg = "";
@@ -201,6 +202,7 @@
         </tbody>
       </table>
     </form>
+    <hr>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
             integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
