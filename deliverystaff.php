db2a60caee2ac073892f6e1867ad133d6c263740
NiKaHou <willi927927@gmail.com>
2019-12-28 13:25:28 +0800

    外送員做完

--- deliverystaff.php
+++ deliverystaff.php
@@ -2,31 +2,231 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title> YuntechEat </title>
-    <?php
-    $conn = new mysqli("localhost", "root", "", "deliverysystem") or die("連接失敗");
-    ?>
+  <meta charset="UTF-8">
+  <title> YuntechEat </title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+  <style>
+  body {
+    font-family: 'Noto Sans TC', sans-serif;
+  }
+  </style>
 </head>
 
 <body>
-    <h1>YuntechEat 外送員管理頁面</h1>
-    <label>請選擇外送員編號
-        <select name="month">
-            <?php
-            $link = mysqli_connect("localhost","root",
-            "","deliverysystem")
-                or die("連線失敗!<br>");
-            $sql = "SELECT * FROM deliverystaff";
-            mysqli_set_charset($link,"utf8");
-            $result = mysqli_query($link, $sql);
-            while($row = mysqli_fetch_assoc($result)) {
-            echo "<option value=".$row["deliveryStaffID"].">".$row["deliveryStaffID"]."</option>\n";
-            }	
-		
-	?>
-        </select>
+  <div class="container">
+    <?php
+    $hasMsg = isset($_GET["msg"]);
+    $isQuery = isset($_GET["query"]);
+    $isEdit = isset($_GET["edit"]);
+    $isCreate = isset($_GET["create"]);
+    if ($isEdit) {
+      $editID = $_GET["edit"];
+    }
+    $isUpdate = isset($_GET["update"]);
+    if ($isUpdate) {
+      $deliveryStaffID = $_GET["update"]; // update this on where
+      $name = $_GET["name"];
+      $tel = $_GET["tel"];
+      // update code in here;
+      $link = mysqli_connect("localhost","root", "","deliverysystem") or die(header("Location: deliverystaff.php?msg=-2".(($isQuery)?"&query=".$_GET["query"]:"")));
+      $sql = "UPDATE deliverystaff SET name = '".$name."' , tel = '".$tel."' WHERE deliveryStaffID = '".$deliveryStaffID."'";
+      // $sql = "SELECT * FROM member";
+      mysqli_set_charset($link, "utf8");
+      try{
+        mysqli_query($link, $sql);
+      }catch(Exception $e){
+        header("Location: deliverystaff.php?msg=-1".(($isQuery)?"&query=".$_GET["query"]:""));
+      }
+      mysqli_close($link);
+      // header("Location: deliverystaff.php?msg=-1".(($isQuery)?"&query=".$_GET["query"]:""));
+      header("Location: deliverystaff.php?msg=0".(($isQuery)?"&query=".$_GET["query"]:""));
+    }
+    $isDelete = isset($_GET["delete"]);
+    if ($isDelete) {
+      $deliveryStaffID = $_GET["delete"];
+      // delete code in here
+      $link = mysqli_connect("localhost","root", "","deliverysystem") or die(header("Location: deliverystaff.php?msg=-2".(($isQuery)?"&query=".$_GET["query"]:""))); 
+      $sql = "DELETE FROM deliverystaff WHERE deliveryStaffID = '".$deliveryStaffID."'";
+      try{
+        mysqli_query($link, $sql);
+      }catch(Exception $e){
+        header("Location: deliverystaff.php?msg=-1".(($isQuery)?"&query=".$_GET["query"]:""));
+      } 
+      header("Location: deliverystaff.php?msg=1".(($isQuery)?"&query=".$_GET["query"]:""));
+    }
+    $isCreate = isset($_GET["create"]);
+    echo $isCreate;
+    if ($isCreate) {
+      $name = $_GET["nameAdd"];
+      $tel = $_GET["telAdd"];
+      // create code in here;
+      $link = mysqli_connect("localhost","root", "","deliverysystem") or die(header("Location: deliverystaff.php?msg=-2".(($isQuery)?"&query=".$_GET["query"]:""))); 
+      $sql = "INSERT INTO deliverystaff VALUES(NULL,'".$name."','".$tel."')";
+      try{
+        mysqli_query($link, $sql);
+      }catch(Exception $e){
+        header("Location: deliverystaff.php?msg=-1".(($isQuery)?"&query=".$_GET["query"]:""));
+      } 
+      // header("Location: deliverystaff.php?msg=2".(($isQuery)?"&query=".$_GET["query"]:""));
+    }
+  ?>
+    <h1 class="text-center mt-2">YuntechEat 外送員管理頁面</h1>
+    <hr>
+    <?php
+      if ($hasMsg) {
+        $msg = "";
+        switch ($_GET["msg"]) {
+          case 0:
+            $msg = "編輯成功";
+            echo '<div class="alert alert-success" style="width: 100%;" role="alert">
+                '.$msg.'
+              </div>';
+            break;
+          case 1:
+            $msg = "刪除成功";
+            echo '<div class="alert alert-success" style="width: 100%;" role="alert">
+                '.$msg.'
+              </div>';
+            break;
+          case 2:
+            $msg = "新增成功";
+            echo '<div class="alert alert-success" style="width: 100%;" role="alert">
+                '.$msg.'
+              </div>';
+            break;
+          case -1:
+            $msg = "錯誤操作";
+            echo '<div class="alert alert-danger" style="width: 100%;" role="alert">
+                '.$msg.'
+              </div>';
+            break;
+          case -2:
+            $msg = "無法連接資料庫";
+            echo '<div class="alert alert-danger" style="width: 100%;" role="alert">
+                '.$msg.'
+              </div>';
+            break;
+          default:
+            # code...
+            break;
+        }
+        
+      }
+    ?>
+    <form class="form-inline" action="deliverystaff.php" method="GET">
+      <div class="form-group mx-sm-3 mb-2">
+        <label for="inputquery" class="sr-only">Account</label>
+        <input name="query" type="text" class="form-control" id="inputquery" placeholder="外送員ID" required
+               value="<?php echo (($isQuery)?$_GET["query"]:"") ?>">
+      </div>
+      <button type="submit" class="btn btn-primary mb-2 mr-2">查詢</button>
+      <?php 
+      if ($isQuery) {
+        echo '<button type="reset" class="btn btn-secondary mb-2 mr-2" onClick="document.location=\'deliverystaff.php\';">取消查詢</button>';
+      } 
+    ?>
+    </form>
 
+    <form class="form-inline" action="deliverystaff.php" method="GET">
+      <?php
+        if ($isQuery) {
+          echo '<input name="query" type="text" class="form-control d-none" value="'.$_GET["query"].'">';
+        }
+      ?>
+
+      <table class="table table-striped table-hover table-sm">
+        <thead>
+          <tr>
+            <th class="align-middle text-center" scope="col">編號</th>
+            <th class="align-middle text-center" scope="col">姓名</th>
+            <th class="align-middle text-center" scope="col">電話</th>
+            <th class="align-middle text-center" scope="col">編輯</th>
+            <th class="align-middle text-center" scope="col">刪除</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $link = mysqli_connect("localhost","root", "","deliverysystem") or die("連線失敗!<br>");
+            if ($isQuery) {
+              $sql = "SELECT * FROM deliverystaff WHERE deliveryStaffID LIKE '%".$_GET["query"]."%'";
+            } else {
+              $sql = "SELECT * FROM deliverystaff";
+            }
+            mysqli_set_charset($link, "utf8");
+            $result = mysqli_query($link, $sql);
+            if (mysqli_num_rows($result) == 0) {
+              echo '<tr><td class="align-middle text-center" colspan="5">查無結果</td></tr>';  //欄位數
+            } else {
+              while($row = mysqli_fetch_assoc($result)) {
+                if ($isEdit && $editID == $row["deliveryStaffID"]) {
+                  //編輯中
+                  echo '<tr>
+                    <th class="align-middle text-center" scope="row">'.$row["deliveryStaffID"].'</th>
+                    <td class="align-middle text-center">
+                      <input name="name" type="text" class="form-control form-control-sm" style="width: 120px;" value="'.$row["name"].'">
+                    </td>
+                    <td class="align-middle text-center">
+                      <input name="tel" type="text" class="form-control form-control-sm" style="width: 120px;" value="'.$row["tel"].'">
+                    </td>
+                    <td class="align-middle text-center" colspan="2">
+                      <button class="btn btn-success btn-sm btn-block" type="submit" name="update" value="'.$row["deliveryStaffID"].'">
+                        送出
+                      </button>
+                    </td>
+                  </tr>';
+                } else {
+                  //一般資訊
+                  echo '<tr>
+                    <th class="align-middle text-center" scope="row">'.$row["deliveryStaffID"].'</th>
+                    <td class="align-middle text-center">'.$row["name"].'</td>
+                    <td class="align-middle text-center">'.$row["tel"].'</td>
+                    <td class="align-middle text-center">
+                      <button class="btn btn-warning btn-sm btn-block" type="submit" name="edit" value="'.$row["deliveryStaffID"].'">
+                        編輯
+                      </button>
+                    </td>
+                    <td class="align-middle text-center">
+                      <button class="btn btn-danger btn-sm btn-block" type="submit" name="delete" value="'.$row["deliveryStaffID"].'">
+                        刪除
+                      </button>
+                    </td>
+                  </tr>';
+                }
+              }
+            }
+          ?>
+          <tr>
+            <!-- 新增欄位 -->
+            <th class="align-middle text-center" scope="row">+</th>
+            <td class="align-middle text-center">
+              <input name="nameAdd" type="text" class="form-control form-control-sm" style="width: 120px;">
+            </td>
+            <td class="align-middle text-center">
+              <input name="telAdd" type="text" class="form-control form-control-sm" style="width: 120px;">
+            </td>
+            <td class="align-middle text-center" colspan="2">
+              <button class="btn btn-info btn-sm btn-block" type="submit" name="create">
+                新增
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+    <hr>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+  </div>
 </body>
 
 </html>
